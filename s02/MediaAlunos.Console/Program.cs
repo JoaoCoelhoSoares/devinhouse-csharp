0e0403014fc940d4f4e2420f5d7c2aeff963e972
JoaoCoelhoSoares <joao_coelho-soares@estudante.sesisenai.org.br>
2022-06-08 17:52:02 -0300

    S02E01-02

---
+++ s02/MediaAlunos.Console/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+float[] notas = new float[6];
+int notaMaiorSete = 0;
+int notaMenorSete = 0;
+int notaMaiorZero = 0;
+int notaZero = 0;
+float notaMedia = 0;
+
+Console.Write("Informe a nota do primeiro aluno: ");
+notas[0] = float.Parse(Console.ReadLine());
+
+Console.Write("Informe a nota do segundo aluno: ");
+notas[1] = float.Parse(Console.ReadLine());
+
+Console.Write("Informe a nota do terceiro aluno: ");
+notas[2] = float.Parse(Console.ReadLine());
+
+Console.Write("Informe a nota do quarto aluno: ");
+notas[3] = float.Parse(Console.ReadLine());
+
+Console.Write("Informe a nota do quinto aluno: ");
+notas[4] = float.Parse(Console.ReadLine());
+
+Console.Write("Informe a nota do sexto aluno: ");
+notas[5] = float.Parse(Console.ReadLine());
+
+
+foreach (var nota in notas)
+{
+
+    notaMedia = notaMedia + nota;
+
+    if (nota > 7)
+    {
+        notaMaiorSete++;
+
+    }
+
+    if (nota < 7)
+    {
+        notaMenorSete++;
+
+    }
+
+    if (nota > 0)
+    {
+        notaMaiorZero++;
+    }
+
+    if (nota == 0)
+    {
+        notaZero++;
+    }
+
+}
+
+notaMedia = notaMedia / notas.Length;
+
+Console.WriteLine($"Nota média da turma: {notaMedia}");
+Console.WriteLine($"Alunos com nota maior que 7: {notaMaiorSete}");
+Console.WriteLine($"Alunos com nota menor que 7: {notaMenorSete}");
+Console.WriteLine($"Alunos com nota maior que 0: {notaMaiorZero}");
+Console.WriteLine($"Alunos com nota igual a 0: {notaZero}");
---
+++ s02/salarioFuncionario.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int anoAtual = 2022;
+
+Console.Write("Informe o nome do funcionário: ");
+string nome = Console.ReadLine();
+
+Console.Write("Informe o ano de nascimento do funcionário: ");
+int anoNascimento = int.Parse(Console.ReadLine());
+
+Console.Write("Informe o salário do funcionário: ");
+float salario = float.Parse(Console.ReadLine());
+
+Console.Write("Informe o % de reajuste do funcionário: ");
+float reajuste = float.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Olá {nome}, você possui {anoAtual - anoNascimento} idade. Tendo em vista o reajuste de {reajuste}%, seu salário será R${salario + (salario * reajuste / 100)}.");
