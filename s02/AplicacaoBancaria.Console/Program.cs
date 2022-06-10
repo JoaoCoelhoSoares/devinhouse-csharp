cae28111ea6ae299e6bdac6ef6937cf50366e7a2
JoaoCoelhoSoares <joao_coelho-soares@estudante.sesisenai.org.br>
2022-06-10 18:06:51 -0300

    Add AplicacaoBancaria

---
+++ s02/AplicacaoBancaria.Console/Program.cs
@@ -0,0 +1,122 @@
+﻿public class Program
+{
+    private static List<string> listaOperacoes = new List<string>();
+    private static int saldo = 0;
+
+    public static void Main(string[] args)
+    {
+
+        Menu();
+
+    }
+
+    private static void Menu()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("\n ------DevInBank------");
+        Console.WriteLine("\n Operação disponíveis:");
+        Console.WriteLine("1 - Consulta saldo");
+        Console.WriteLine("2 - Depósito");
+        Console.WriteLine("3 - Saque");
+        Console.WriteLine("4 - Histórico");
+        Console.WriteLine("5 - Sair");
+
+        Console.Write("\n Selecione o tipo de operação: ");
+        string opcaoSelecionada = Console.ReadLine();
+
+        if (opcaoSelecionada == "5")
+        {
+            Console.WriteLine("\nObrigado por ser cliente do DevInBank.");
+            return;
+        }
+        else
+        {
+            Executar(opcaoSelecionada);
+            Menu();
+        }
+    }
+
+    private static void Executar(string resposta)
+    {
+        switch (resposta)
+        {
+            case "1":
+                Console.WriteLine($"\nO saldo atual é: {saldo}");
+                break;
+
+            case "2":
+                Deposito();
+                break;
+
+            case "3":
+                Saque();
+                break;
+
+            case "4":
+                Histórico();
+                break;
+
+            default:
+                break;
+        }
+
+
+    }
+
+    private static void Deposito()
+    {
+        Console.Write("\n Informe o valor do depósito: ");
+        int valorDeposito = int.Parse(Console.ReadLine());
+
+        if (valorDeposito < 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\nOperação inválida! O valor informado ({valorDeposito}) é negativo.");
+        }
+        else
+        {
+            saldo = saldo + valorDeposito;
+            listaOperacoes.Add($"- Depósito no valor de R${valorDeposito}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\nDeposito no valor {valorDeposito} realizado com sucesso.");
+        }
+    }
+
+    private static void Saque()
+    {
+        Console.Write("\n Informe o valor do depósito: ");
+        int valorSaque = int.Parse(Console.ReadLine());
+
+        if (saldo - valorSaque < 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\nOperação inválida! Você não possui saldo suficiente.");
+        }
+        else
+        {
+            saldo = saldo - valorSaque;
+            listaOperacoes.Add($"- Saque no valor de R${valorSaque}");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\nSaque no valor {valorSaque} realizado com sucesso.");
+        }
+
+    }
+
+    private static void Histórico()
+    {
+        foreach (var operacao in listaOperacoes)
+        {
+            Console.WriteLine(operacao);
+        }
+    }
+
+    public enum OpcoesMenu
+    {
+        Saldo,  // 0
+        Deposito,  // 1
+        Saque,  // 2
+        Historico,    //3
+        Sair    // 4
+    }
+}
